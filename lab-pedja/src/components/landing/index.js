38c455204d3d8cd504d5bfe95bfec2abb70417ee
Predrag Josifovic <pjosifovic@yahoo.com>
2018-01-10 15:39:34 -0800

    adding landing/index.js

---
+++ lab-pedja/src/components/landing/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className='landing'>
+        <h3>
+          TODO Tracker is a simple react app that lists everything that you have to do. By keeping such a list, you make sure that your tasks are written down all in one place so you don't forget anything important
+        </h3>
+      </div>
+    )
+  }
+}
+
+export default Landing;

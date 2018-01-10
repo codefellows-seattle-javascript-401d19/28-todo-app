b1856eb6256eba499f7b4bb6f774b7aaf2b5bd37
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-10 15:01:25 -0800

    add landing page component

--- lab-shannon/src/components/app/index.js
+++ lab-shannon/src/components/app/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
+import Landing from '../landing';
 
 class App extends React.Component {
   render(){
@@ -14,7 +15,7 @@ class App extends React.Component {
               <li><Link to='/'>Home</Link></li>
               <li><Link to='/dashboard'>Dashboard</Link></li>
             </ul>
-            <Route exact path = '/' component={() => <h1>hi</h1>}/>
+            <Route exact path = '/' component={Landing}/>
             <Route exact path = '/dashboard' component={() => <h1>bye</h1>}/>
           </div>
         </BrowserRouter>
---
+++ lab-shannon/src/components/landing/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Landing extends React.Component{
+  render(){
+    return(
+      <p>Want to get more organized? Then have you come to the right place! This app lets you create a list of notes you would like to complete in order to help you achieve your goals.</p>
+    );
+  }
+}
+
+export default Landing;

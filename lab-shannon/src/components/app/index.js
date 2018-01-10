c346d95e3129dadf36e9010dc939c2b13ee18603
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-10 14:42:20 -0800

    two links on the page which change view

--- lab-shannon/src/components/app/index.js
+++ lab-shannon/src/components/app/index.js
@@ -1,1 +1,26 @@
 import React from 'react';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div className='app'>
+        <BrowserRouter>
+          <div>
+            <header>
+              <h1>Welcome to Your To Do List</h1>
+            </header>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/dashboard'>Dashboard</Link></li>
+            </ul>
+            <Route exact path = '/' component={() => <h1>hi</h1>}/>
+            <Route exact path = '/dashboard' component={() => <h1>bye</h1>}/>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
--- lab-shannon/src/main.js
+++ lab-shannon/src/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-let content = document.createElement('div');
+const content = document.createElement('div');
 document.body.appendChild(content);
 
-// ReactDOM.render(<App />, content);
+ReactDOM.render(<App />, content);

28d38745f1c2b44c539f0f0eabbc009c2370e4b5
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-10 13:45:24 -0800

    add index.html file and fill in webpack config file

--- lab-shannon/webpack.config.js
+++ lab-shannon/webpack.config.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/src/main.js`,
+  output: {
+    filename: 'bundle.[hash].js',
+    path: `${__dirname}/src/build`,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+    }),
+    new ExtractPlugin(`bundle.[hash].scss`),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+    ],
+  },
+};

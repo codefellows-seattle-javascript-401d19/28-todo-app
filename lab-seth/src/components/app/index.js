97660f9b975103dcc8d4fd6d4a678ff03cf99aa3
SethDonohue <donohue.seth@gmail.com>
2018-01-10 17:32:42 -0800

    basic compile passes

--- lab-seth/src/components/app/index.js
+++ lab-seth/src/components/app/index.js
@@ -11,6 +11,20 @@ class App extends React.Component {
           <div>
             <header>
               <h1> To Do Checklist </h1>
-    )
+              <nav>
+                <ul>
+                  <li><Link to='/'>Home</Link></li>
+                  <li><Link to='/dashboard'>dashboard</Link></li>
+                </ul>
+              </nav>
+            </header>
+            <Route exact path='/' component={() => <h1> Welcome to the Home page for our Basic To Do list App! </h1>} />
+            <Route exact path='/dashboard' component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
+
+export default App;

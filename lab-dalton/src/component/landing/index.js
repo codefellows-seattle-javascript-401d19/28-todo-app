3c8145af9ca13b512a162afcbc9dba112f6fc0a9
Dalton Carr <daltonlcarr@yahoo.com>
2018-01-10 20:08:13 -0800

    simple landing page

---
+++ lab-dalton/src/component/landing/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className='landing'>
+        <p>Welcome to the Todo List Application! This is a frontend application where you can create your own Todo List. To start, please click on Dashboard from the navigation bar. 
+          <br></br>         
+        Once you have completed your task, you can delete it from your list.</p>
+        <br></br>
+
+        <h2 className='github'>Dalton Carr</h2>
+      </div>
+    );
+  }
+}
+
+export default Landing;

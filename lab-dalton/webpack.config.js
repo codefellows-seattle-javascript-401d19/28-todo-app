3673735a936c6b29f0d95082580fb57e2aad0f32
Dalton Carr <daltonlcarr@yahoo.com>
2018-01-10 20:01:20 -0800

    new webpack config file from today

--- lab-dalton/webpack.config.js
+++ lab-dalton/webpack.config.js
@@ -1,30 +1,49 @@
-const path = require('path');
+'use strict';
 
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const htmlWebpackPluginConfig = new htmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: '#content',
-});
+const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve('build'),
-    filename: 'bundle.[hash].js',
-  },
-  plugins: [htmlWebpackPluginConfig],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader',
-      },
-    ],
-  },
+const webPackConfig = module.exports = {};
+
+webPackConfig.entry = `${__dirname}/src/main.js`;
+webPackConfig.output = {
+  filename : 'bundle.[hash].js',
+  path : `${__dirname}/build`,
+};
+
+webPackConfig.plugins = [
+  new HTMLPlugin(),
+  new ExtractTextPlugin('bundle.[hash].css'),
+];
+
+webPackConfig.module = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader', 
+    },
+    {
+      test:  /\.scss$/,
+      loader: ExtractTextPlugin.extract({
+        use: [
+          'css-loader', 
+          'resolve-url-loader', 
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true, 
+              includePaths: [`${__dirname}/src/style`],
+            },
+          },
+        ],
+      }),
+    },
+  ],
+};
+
+webPackConfig.devtool = 'eval-source-map';
+
+webPackConfig.devServer = {
+  historyApiFallback: true,
 };

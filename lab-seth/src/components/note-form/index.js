273ba8874a73b70ece41bb53747e1e19ff363cbe
SethDonohue <donohue.seth@gmail.com>
2018-01-11 13:55:14 -0800

    note-form refactored

--- lab-seth/src/components/note-form/index.js
+++ lab-seth/src/components/note-form/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 const uuidv1 = require ('uuid/v1');
 import uuid from 'uuid';
 
+let newState = {
+  title: '',
+  content: '',
+  editing: true,
+  completed: false,
+};
+
+
 class NoteForm extends React.Component {
   constructor(props){
     super(props);
 
-    this.state = {
-      title: '',
-      content: '',
-      editing: true,
-      completed: false,
-    };
+    this.state = this.props.note ? this.props.note : newState;
 
     //=======================================================
     // Handlers
@@ -30,18 +33,18 @@ class NoteForm extends React.Component {
   handleSubmit(event){
     event.preventDefault();
 
-    this.props.onComplete({
-      id: uuidv1(),
-      title : event.target.title.value,
-      content : event.target.content.value,
-      editing : false,
-      completed: true,
-    });
-
+    this.props.onComplete(this.state);
     this.setState({
       title: '',
-      content: '',
+      content :'',
     });
+    // this.props.onComplete({
+    //   id: uuidv1(),
+    //   title : event.target.title.value,
+    //   content : event.target.content.value,
+    //   editing : false,
+    //   completed: true,
+    // });
   }
 
   handleChange(event){
@@ -55,15 +58,19 @@ class NoteForm extends React.Component {
   //=======================================================
   // Hooks
 
+  componentWillReceiveProps(nextProps){
+    if(nextProps.note)
+      this.setState(nextProps.note);
+  }
+
   render(){
+    let buttonText = this.props.note ? 'Update' : 'Add';
     return(
-      <div>
       <form className='note-form' onSubmit={this.handleSubmit}>
         <input type='text' name='title' placeholder='title' value={this.state.title} onChange={this.handleChange} />
         <input type='text' name='content' placeholder='content' value={this.state.content} onChange={this.handleChange} />
-        <button type='submit'>Create Note</button>
+        <button type='submit'>{buttonText}</button>
       </form>
-      </div>
     );
   }
 }

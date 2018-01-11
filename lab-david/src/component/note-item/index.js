96b697f053abb9370b4e5970d270ac9fbce3b86d
David Lindahl <davidalindahl@gmail.com>
2018-01-11 09:36:21 -0800

    removing corpse code

--- lab-david/src/component/note-item/index.js
+++ lab-david/src/component/note-item/index.js
@@ -3,30 +3,6 @@ import React from 'react';
 class NoteItem extends React.Component{
 
   //-----------------------------------------
-  // Member functions 
-  //-----------------------------------------
-  // handleSubmit(event){
-  //   event.preventDefault();
-
-  //   this.props.handleAddNote(this.state);
-  //   this.setState({
-  //     title : '',
-  //     content : '',
-  //   });
-
-  //   handleDelete(event){
-  
-  //     if(this.props.state.item ===);
-  //     this.setState({
-  //       title : '',
-  //       content : '',
-  //     });
-
-
-  // }
-
-
-  //-----------------------------------------
   // Hooks
   //-----------------------------------------
 

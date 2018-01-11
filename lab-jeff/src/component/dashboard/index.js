51b47670b3a6f692934d50010570a5452cf66266
jjkusowski <jjkusowski@yahoo.com>
2018-01-10 16:09:38 -0800

    saving progress.  dashboard and note-form work

---
+++ lab-jeff/src/component/dashboard/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import NoteForm from '../note-form';
+const uuidv1 = require('uuid/v1');
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [],
+    };
+
+    let memberFunctions = Object.getOwnPropertyNames(Dashboard.prototype);
+    for (let functionName of memberFunctions) {
+      if (functionName.startsWith('handle')) {
+        this[functionName] = this[functionName].bind(this);
+      }
+    }
+  }
+
+  handleAddNote(note) {
+    note.id = uuidv1();
+    note.editing = false;
+    note.completed = false;
+
+    this.setState(previousState => {
+      return { notes: [...previousState.notes, note] };
+    });
+  }
+
+  handleRemoveNote(note) {
+    this.setState(previousState => {
+      return { notes: previousState.notes.filter(item => item.id !== note.id) };
+    });
+  }
+  render() {
+    return (
+      <div className="dashboard">
+        <p>This is the dashboard</p>
+        <NoteForm handleAddNote={this.handleAddNote} />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;

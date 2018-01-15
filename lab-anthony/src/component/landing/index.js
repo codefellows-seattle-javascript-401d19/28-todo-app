4469ed1f00284412f6c5c41f482d1d7ad67975ec
Anthony Robinson <robinsonamk@gmail.com>
2018-01-15 12:47:59 -0800

    small changes

--- lab-anthony/src/component/landing/index.js
+++ lab-anthony/src/component/landing/index.js
@@ -3,9 +3,19 @@ import React from 'react';
 class Landing extends React.Component {
   render(){
     return (
-      <div>
-        <h1>Note Maker Thing</h1>
-        <h3>Here at Note Maker Thing, we pride ourselves on good note taking. <p> please anjoy the app! </p></h3>
+      <div className="landing">
+        <h1>
+          Note Maker Thing
+        </h1>
+        <h3>
+          Here at Note Maker Thing, we pride ourselves on good note taking.
+          <p>
+          Click on the dashboard link to create some notes.
+          </p>
+          <p>
+          please anjoy the app!
+          </p>
+        </h3>
       </div>
     );
   }

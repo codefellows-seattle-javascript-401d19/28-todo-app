ba82f4665a981df8d29568da7196cd7f8dfeb99c
Predrag Josifovic <pjosifovic@yahoo.com>
2018-01-17 15:26:22 -0800

    escape char &#39

--- lab-pedja/src/components/landing/index.js
+++ lab-pedja/src/components/landing/index.js
@@ -5,7 +5,7 @@ class Landing extends React.Component {
     return (
       <div className="landing">
         <p>
-          TODO Tracker is a simple react app that lists everything that you have to do. By keeping such a list, you make sure that your tasks are written down all in one place so you don&apos;t forget anything important. 
+          TODO Tracker is a simple react app that lists everything that you have to do. By keeping such a list, you make sure that your tasks are written down all in one place so you don&#39;t forget anything important. 
         </p>
         <p>
           Click on DASHBOARD and start adding your tasks. Delete it once task is completed. 
